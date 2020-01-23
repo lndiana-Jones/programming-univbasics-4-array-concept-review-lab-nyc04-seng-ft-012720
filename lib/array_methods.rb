ec545a73ee9bb990e59a0f476291d623cbf945ba
Andrew D'Antonio <andrewdantonio10@gmail.com>
2020-01-23 02:49:00 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,7 +4,7 @@ def find_element_index(array, value_to_find)
   
   while array[i] do
     if array[i] == value_to_find
-      return array[value_to_find)
+      return array[value_to_find]
     end 
     i += 1 
   end
